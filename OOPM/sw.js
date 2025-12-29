7c658fdbeb03dc9ced211ddb61443dfdc417912c
Amey Thakur <ameythakur20@gmail.com>
2025-12-29 04:09:24 -0500

    OOPM

--- OOPM/sw.js
+++ OOPM/sw.js
@@ -1,3 +1,19 @@
+/**
+ * ================================================================
+ *   Object-Oriented Programming (OOPM) Lab Portfolio - Service Worker
+ * ================================================================
+ *   Author: Amey Thakur
+ *   GitHub: https://github.com/Amey-Thakur
+ *   Course: Object-Oriented Programming (OOPM) Lab
+ *   Roll No: 50
+ *   Batch: B3
+ *   Repository: https://github.com/Amey-Thakur/OOPM-JAVA-LAB
+ *   Date: January 17, 2020
+ *   
+ *   Description: Service Worker for PWA offline capabilities and caching.
+ * ================================================================
+ */
+
 const CACHE_NAME = 'oopm-portfolio-v1';
 const ASSETS = [
     'index.html',
